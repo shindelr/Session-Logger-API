dabb710d9cc0a850e6878e038ffdb38a6e45eb08
shindelr <129821483+shindelr@users.noreply.github.com>
2024-03-28 21:27:43 -0700

    Big things are happening. In this commit: Cleared out those extra package.json, package-lock.json, and node_modules files which were hanging out in the root directory but not being used for anything. Built out the NDBC meteorlogical data API and threaded it so that it pings a list of stations once an hour, dumping the raw data into a JSON file. Asynchronized the form submission on the frontend to send the form data back to the flask app and it's receiving just fine.

---
+++ session-logger-backend/server.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from time import sleep
+from pandas import read_csv
+from json import dump
+from threading import Thread
+
+
+# APP SETUP
+app = Flask(__name__)
+CORS(app)
+port_num = 5001
+
+
+# ROUTES
+
+@app.route('/session_form_submission', methods=['POST'])
+def session_form_submission():
+    """
+    Receives the HTTP POST request from the client's session logger form
+    submission. 
+    :return:
+        A success message for successful connectino.
+    """
+    sleep(1)
+    data = request.json
+    print(data)
+    return jsonify({'message': 'Form received successfully'})
+
+
+# UTILITIES
+
+def dump_meteorlogical_NBDC_data(station):
+    """
+    Retrieve the most recent meteorlogical data from NDBC station 46050.
+    :params:
+        station -- str representing buoy station number.
+    """
+    url = 'https://www.ndbc.noaa.gov/data/realtime2/{}.txt'.format(station)
+
+    try:
+        data = read_csv(url, sep='\s+')
+        with open('./test.json', 'w') as f:
+            dump(data.iloc[1:, :].to_dict(orient='records'), f)
+        print('Success: Meteorlogical data dump')
+
+    except Exception as e:
+        print('Failure: Meteorlogical data, ' + str(e))
+
+
+def ping_station(station):
+    """
+    Ping the desired station once an hour to stay updated.
+    :params:
+        station -- str representing buoy station number.
+    """
+    while True:
+        print('Fetching meteorlogical data now.')
+        dump_meteorlogical_NBDC_data(station)
+        sleep(3600)
+
+
+def initialize_meteorlogical_thread(station_list):
+    """
+    Initialize the daemonized threads which will periodically ping the NDBC buoy
+    stations for their data.
+    :params:
+        station_list -- List[str], list of strings representing buoy numbers.
+    """
+    for station in station_list:
+        # Create the thread targeting the ping_station function
+        station_thread = Thread(target=ping_station, args=(station, ))  # args expects tuple
+        # Designate it a daemon
+        station_thread.daemon = True
+        # Initialize the daemonized thread.
+        station_thread.start()
+
+
+if __name__ == '__main__': 
+    available_stations = ['46050']
+    initialize_meteorlogical_thread(available_stations)
+
+    app.run(debug=True, port=port_num)
+    print(f'Running on port {port_num}')
