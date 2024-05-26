385776688438debf9e18463aaf4d937d31fc7110
shindelr <129821483+shindelr@users.noreply.github.com>
2024-05-25 21:52:01 -0700

    Started in on data processing

--- session-logger-backend/src/dataProc.py
+++ session-logger-backend/src/dataProc.py
@@ -6,7 +6,10 @@ from metric or degree to standard or cardinal, etc..
 from pint import UnitRegistry, Quantity
 
 class UnitConverter:
-    """Handle unit conversion."""
+    """
+    Unit conversion utility. Use to convert various heights, speeds, 
+    directions.
+    """
 
     def __init__(self) -> None:
         # Pint library unit converter
@@ -15,7 +18,8 @@ class UnitConverter:
         self.ureg_temp = UnitRegistry()
         self.ureg_temp.default_format = '.3f'
 
-    def find_cardinal_direction(self, value):
+
+    def find_cardinal_direction(self, value: float) -> str:
         """
         Convert direction in degrees to its corresponding cardinal direction.
         Works by dividing the given value by 45 (since this func uses 8 
@@ -29,42 +33,81 @@ class UnitConverter:
         cardinals = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
         res = cardinals[((int(value + 45)) % 360) // 45]
         return res
-    
-    def meters_to_feet(self, value):
+
+
+    def meters_to_feet(self, value: float) -> float:
         """
         Conver meters to feet.
         :param:
             - value: float currenty in meters \n
         :return:
-            - String representing the value in feet.
+            - Float representing the value in feet.
         """
         meters = value * self.ureg.meter
         feet = meters.to(self.ureg.foot)
-        return str(round(feet.magnitude, 1))
+        return round(feet.magnitude, 1)
 
-    def meters_per_sec_to_mph(self, value):
+
+    def feet_to_meters(self, value: float) -> float:
+        """
+        Convert feet to meters.
+        :param:
+            - value: float currenty in feet \n
+        :return:
+            - Float representing the value in meters.
+        """
+        feet = value * self.ureg.foot
+        meters = feet.to(self.ureg.meter)
+        return round(meters.magnitude, 1)
+
+
+    def meters_per_sec_to_mph(self, value: float) -> float:
         """
         Convert m/s to mph.
         :param:
             - value: float currently in m/s \n
         :return:
-            - String representing the value converted to mph.
+            - Float representing the value converted to mph.
         """
         # value * m/s
         m_per_sec = value * (self.ureg.meter / self.ureg.second)
         mph = m_per_sec.to(self.ureg.mile / self.ureg.hour)
-        return str(round(mph.magnitude, 1))
+        return round(mph.magnitude, 1)
 
+
+    # Quantity class isn't letting me use type hints
     def celsius_to_fahrenheit(self, value):
         """
         Convert Celsius values to Fahrenheit.
         :param:
             - value: a float currently in degrees Celsius \n
         :return:
-            - String representing the value converted to F.
+            - Quantity object representing the value converted to F.
         """
         # Convert value to a Pint Quantity object first
         celsius = Quantity(value, self.ureg_temp.degC)
-        F = celsius.to(self.ureg_temp.degF)
-        # Make the float manageable and convert to a string for displaying later
-        return str(round(F.magnitude, 1))
+        fahrenheit = celsius.to(self.ureg_temp.degF)
+        return round(fahrenheit.magnitude, 1)
+
+
+    def fahrenheit_to_celsius(self, value):
+        """
+        Convert Fahrenheit values to Celsius.
+        :param:
+            - value: a float currently in degrees Fahrenheit \n
+        :return:
+            - Quantity object representing the value converted to Celsius.
+        """
+        # Convert value to a Pint Quantity object first
+        fahrenheit = Quantity(value, self.ureg_temp.degF)
+        celsius = fahrenheit.to(self.ureg_temp.degC)
+        return round(celsius.magnitude, 1)
+
+
+class BuoyDataCleaner:
+    """
+    Data cleaning utility. Do things like excise useless data columns, convert
+    units, etc.
+    """
+    def __init__(self) -> None:
+        self.unit_conv = UnitConverter()
